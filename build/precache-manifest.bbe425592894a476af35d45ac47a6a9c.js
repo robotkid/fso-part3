84824984c27f04f891f553b06976c52267945b5b
Dan Horwood <dpj@sent.com>
2019-06-14 13:56:23 +0800

    part 3 exercise 3.10

---
+++ build/precache-manifest.bbe425592894a476af35d45ac47a6a9c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46c729a26de8f300cb41b172edbe496e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "dad61d7cd8517b78782f",
+    "url": "/static/css/main.42c606d0.chunk.css"
+  },
+  {
+    "revision": "656ac7b39ea94196184f",
+    "url": "/static/js/2.e9593a85.chunk.js"
+  },
+  {
+    "revision": "dad61d7cd8517b78782f",
+    "url": "/static/js/main.f6205421.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
