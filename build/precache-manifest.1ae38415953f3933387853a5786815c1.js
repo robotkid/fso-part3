f5c1fc70f7ad63941bb7d4da56237d6de37aba2d
Dan Horwood <dpj@sent.com>
2019-08-19 09:36:43 +0800

    added new fronted files

---
+++ build/precache-manifest.1ae38415953f3933387853a5786815c1.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "773d4a324416791dd646b997c61c1950",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bebf95aa3e2b470260c1",
+    "url": "/static/css/main.42c606d0.chunk.css"
+  },
+  {
+    "revision": "6957d2614f6fa148e7d4",
+    "url": "/static/js/2.6e1695dd.chunk.js"
+  },
+  {
+    "revision": "bebf95aa3e2b470260c1",
+    "url": "/static/js/main.0cacb497.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
